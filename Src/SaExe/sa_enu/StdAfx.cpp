e22d1816ed51e15805d06133e26a89949f426264
kent_gorham <kent_gorham@874b31ba-64dd-0310-97f8-c2947527d039>
2013-03-18 19:44:38 +0000

    SA - 3.1.0.41 - 3/18/2013 1) Added Ctrl+Q accelerator for 'Add Phonetic Segment' command.  Left in 'Alt+=' 2) Remapped Ctrl+Q (segment size) to Ctrl+E 3) Fixed menu enable/disable problem for Ctrl+C (copy) menu item. 4) Added Alt+2 control sequence to automatically add PL2 segment. 5) Relabled and retargeted 'Phrase L2' button on Transcription Editor to 'Phrase L1' 6) Add Alt+Z control sequence to automatically add phonetic, gloss, PL1 and PL2 segments 7) Fixed moving playback cursor problem that occurs after pause button is placed on player dialog.

--- Src/SaExe/sa_enu/StdAfx.cpp
+++ Src/SaExe/sa_enu/StdAfx.cpp
@@ -1,6 +1,6 @@
 // stdafx.cpp : source file that includes just the standard includes
-//	SA_ENU.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
+//  SA_ENU.pch will be the pre-compiled header
+//  stdafx.obj will contain the pre-compiled type information
 #include "stdafx.h"
 
 
