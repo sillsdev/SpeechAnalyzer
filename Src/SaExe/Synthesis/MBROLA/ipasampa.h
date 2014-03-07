640d8ca1b7e24ee90cb66085b1a188b998cb6465
kent_gorham <kent_gorham@874b31ba-64dd-0310-97f8-c2947527d039>
2014-03-07 05:09:16 +0000

    Flattening project directory structure.

--- Src/SaExe/Synthesis/MBROLA/ipasampa.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IPASAMPA_H
-#define _IPASAMPA_H
-#include <TCHAR.h>
-
-extern "C" int Lookup( const void * Key, const void * TblEntry);
-extern "C" TCHAR * IpaToSampa( const TCHAR * IpaString);
-extern "C" bool QuerySymbol( const TCHAR IpaChar);
-
-#endif
