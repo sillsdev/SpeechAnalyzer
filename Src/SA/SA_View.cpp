a5601ce7e356db6c532a140f541ddee3da1a4756
Darcy Wong <darcy_wong@sil.org>
2022-03-03 13:00:01 +0700

    fix: Use temp wav file on "Save As" for mp3 files (#54)
    * fix: Disable File SaveAs for mp3 files
    
    * fix: rename mp3
    
    * chore: Check temp file is available
    
    * fix: Use original filename
    
    * fix: Re-use original filename

--- Src/SA/SA_View.cpp
+++ Src/SA/SA_View.cpp
@@ -11611,7 +11611,28 @@ void CSaView::OnFileSaveAs() {
 			// get the current view caption string
 			path = doc.GetFilenameFromTitle().c_str();
 		}
-		path = FileUtils::ReplaceExtension((LPCTSTR)path, L".wav").c_str();
+
+		if (FileUtils::EndsWith((LPCTSTR)path, L".mp3")) {
+			// If path is mp3 file, use the temporary converted wavefile already in use:
+			CString tempConvertedWav = doc.GetConvertedWaveFilename().c_str();
+			if (!doc.CheckWaveFormatForOpen(tempConvertedWav)) {
+				app.ErrorMessage(IDS_ERROR_CANT_READ_WAVE_FILE, (LPCTSTR)tempConvertedWav);
+				return;
+			}
+
+			// Reuse the original filename for the new filename
+			wstring originalFilename = FileUtils::GetFilename((LPCTSTR)path);
+			wstring tempFilename = FileUtils::ReplaceExtension((LPCTSTR)originalFilename.c_str(), L".wav");
+			wstring tempFolder = FileUtils::GetParentFolder(doc.GetConvertedWaveFilename().c_str());
+			wstring temp = tempFolder;
+			temp.append(tempFilename.c_str());
+			FileUtils::Copy(doc.GetConvertedWaveFilename().c_str(), temp.c_str());
+			path = (CString)temp.c_str();
+
+			//path = doc.GetConvertedWaveFilename().c_str();
+		} else {
+			path = FileUtils::ReplaceExtension((LPCTSTR)path, L".wav").c_str();
+		}
 		docname = path;
 	}
 
